5a4a0e1f4c4010c3c564471b7006519f56c0f5e7
james <jamesrochabrun@gmail.com>
2017-11-27 22:42:29 -0800

    Initial Commit

---
+++ ProtocolBasedNetworkingTutorialFinal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProtocolBasedNetworkingTutorialFinal
+//
+//  Created by James Rochabrun on 11/27/17.
+//  Copyright © 2017 James Rochabrun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ProtocolBasedNetworkingTutorialFinalTests/ProtocolBasedNetworkingTutorialFinalTests.swift
@@ -0,0 +1,36 @@
+//
+//  ProtocolBasedNetworkingTutorialFinalTests.swift
+//  ProtocolBasedNetworkingTutorialFinalTests
+//
+//  Created by James Rochabrun on 11/27/17.
+//  Copyright © 2017 James Rochabrun. All rights reserved.
+//
+
+import XCTest
+@testable import ProtocolBasedNetworkingTutorialFinal
+
+class ProtocolBasedNetworkingTutorialFinalTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
